0dd53b8e4009c41c16a271ea3591934c839c1529
williamfurquim <w.furquim@aluno.senai.br>
2026-03-04 15:52:54 -0300

    Alterações para fazer deploy no Render

--- ProjetoRNT/Backend/src/app.ts
+++ ProjetoRNT/Backend/src/app.ts
@@ -8,13 +8,12 @@ const app = express();
 
 // Configuração básica do CORS
 app.use(cors({
-  origin: "http://localhost:5173", // ou porta do seu Vite
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
 app.use(routes);
 
-const PORT = 3001;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}/usuarios`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando em ${PORT}`));
